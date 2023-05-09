5a809fffb394123567e811249252cbe4968f626b
fufu78 <40305354+fufu78@users.noreply.github.com>
2023-05-09 11:50:57 +0200

    Add files via upload
    file used to create MCS database framework - shows some of data cleaning process

---
+++ project sql script.sql
@@ -0,0 +1,88 @@
+CREATE DATABASE cfg_project;
+
+# location table
+CREATE TABLE locations (
+location_code INT PRIMARY KEY,
+location_name VARCHAR(50));
+
+INSERT INTO locations 
+(location_code, location_name)
+VALUES
+(1, 'North East'),
+(2, 'North West'),
+(3, 'Yorkshire and The Humber'),
+(4, 'East Midlands'),
+(5, 'West Midlands'),
+(6, 'East of England'),
+(7, 'London'),
+(8, 'South East'),
+(9, 'South West');
+
+SELECT *
+FROM locations;
+
+# family location table
+CREATE TABLE family_location (
+MCS_ID VARCHAR(20) PRIMARY KEY,
+location_code INT);
+
+ALTER TABLE family_location
+ADD CONSTRAINT fk_location_code
+FOREIGN KEY (location_code) REFERENCES locations (location_code);
+
+SELECT *
+FROM family_location;
+
+# remove locations that are outside of england
+DELETE FROM family_location
+WHERE location_code >9;
+
+# health data
+CREATE TABLE health_condition (
+MCS_ID VARCHAR(20) PRIMARY KEY,
+longstanding_illness INT,
+migraine INT,
+hayfever INT,
+bronchitis INT, 
+asthma INT,
+back_pain INT,
+fits_etc INT,
+diabetes INT,
+cancer INT,
+digestive_bowel INT);
+
+ALTER TABLE health_condition
+ADD CONSTRAINT fk_family_code
+FOREIGN KEY (MCS_ID) REFERENCES family_location (MCS_ID);
+
+SELECT *
+FROM health_condition;
+
+# removing data where location data is missing
+DELETE FROM health_condition
+WHERE MCS_ID NOT IN (SELECT f.MCS_ID
+FROM family_location f);
+
+
+CREATE TABLE reported_health_factors (
+MCS_ID VARCHAR(20) PRIMARY KEY,
+general_health_rating INT,
+ecig_vape_usage INT,
+diag_depression_anxiety INT,
+treat_depression_anxiety INT);
+
+ALTER TABLE reported_health_factors
+ADD CONSTRAINT fk_health_code
+FOREIGN KEY (MCS_ID) REFERENCES family_location (MCS_ID);
+
+
+SELECT *
+FROM reported_health_factors;
+
+DELETE FROM reported_health_factors
+WHERE MCS_ID NOT IN (SELECT f.MCS_ID
+FROM family_location f);
+
+
+
+
